2864c304f75304acb128b6aeb680f641571aeb5e
linewon <linewon1893@gmail.com>
2017-12-24 22:10:45 +0800

    spring bean ioc : validateController - restTemplate

--- src/main/java/line/modules/validated/web/ValidatedController.java
+++ src/main/java/line/modules/validated/web/ValidatedController.java
@@ -2,13 +2,19 @@ package line.modules.validated.web;
 
 import java.util.Iterator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSONObject;
 
 import line.modules.validated.entity.request.StringRequest;
 
@@ -16,19 +22,39 @@ import line.modules.validated.entity.request.StringRequest;
 @RequestMapping("/validated")
 public class ValidatedController {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@PostMapping("")
 	public String validation(@RequestBody @Validated StringRequest request, BindingResult result) {
-		
+
 		if (result.hasErrors()) {
 			StringBuilder sb = new StringBuilder();
 			Iterator<ObjectError> itr = result.getAllErrors().iterator();
 			while (itr.hasNext()) {
-				
+
 				sb.append(itr.next().getDefaultMessage()).append(';');
 			}
 			return sb.toString();
 		}
-		
+
 		return "Successfully!";
+	}
+
+	@GetMapping("")
+	public String httpClient() {
+		JSONObject json = new JSONObject();
+		json.put("name", "line");
+		String url = "http://localhost:18080/holy-spring/validated";
+		ResponseEntity<String> responseEntity = null;
+		String resp = null;
+		try {
+			responseEntity = restTemplate.postForEntity(url, json, String.class);
+			resp = responseEntity.getBody();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return resp;
 	}
 }
--- src/test/restTemplate/HttpClientTest.java
+++ src/test/restTemplate/HttpClientTest.java
@@ -1,18 +1,63 @@
 package restTemplate;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSONObject;
 
 public class HttpClientTest {
 
-	@Autowired
 	private RestTemplate restTemplate;
 	
+	public HttpClientTest() {
+//		ClientHttpRequestFactory request = new SimpleClientHttpRequestFactory();
+//		restTemplate = new RestTemplate(request);
+		restTemplate = new RestTemplate();
+		
+		List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
+		try {
+			StringHttpMessageConverter stringConverter = (StringHttpMessageConverter) converters.get(1);
+			MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converters.get(6);
+			if (stringConverter.getDefaultCharset() == jsonConverter.getDefaultCharset())
+				System.out.println("Y Y Y Y YES!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Iterator<HttpMessageConverter<?>> it = restTemplate.getMessageConverters().iterator();
+		while (it.hasNext()) {
+			HttpMessageConverter<?> messageConverter = it.next();
+			if (messageConverter instanceof StringHttpMessageConverter) {
+				Charset cs = ((StringHttpMessageConverter) messageConverter).getDefaultCharset();
+				System.out.println(cs.name());
+				((StringHttpMessageConverter) messageConverter).setDefaultCharset(Charset.forName("utf-8"));;
+				cs = ((StringHttpMessageConverter) messageConverter).getDefaultCharset();
+				System.out.println(cs);
+			}
+		}
+	}
+	
 	public void test1() {
-		restTemplate.postForEntity();
+		String url = "http://localhost:18080/holy-spring/validated";
+		JSONObject postData = new JSONObject();
+		Class<String> clz = String.class;
+		
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, postData, clz);
+		String resp = responseEntity.getBody();
+		System.out.println(resp);
+		
+		
 	}
 	
 	public static void main(String[] args) {
-		
+		HttpClientTest demo = new HttpClientTest();
+		demo.test1();
 	}
 }
